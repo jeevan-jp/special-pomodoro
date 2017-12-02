faff9dae2fed9d092a8cd683eec52c5adbd77645
jeevan-jp <jiwanprakash65@gmail.com>
2017-12-03 00:38:14 +0530

    functioning

--- script.js
+++ script.js
@@ -1,23 +1,74 @@
 $(document).ready(function() {
+
+	$('#sessionText').html('Session Running');
+
+	timer();
+
+	$('#plusBreak').on("click", function() {
+		$('#sessionText').html('Break Time');
+		var x = $('#break').val();
+		if(x < 59) {
+			$('#break').val(parseInt($('#break').val()) + 1);
+			$('#minutes').val(parseInt($('#break').val()));
+			$('#seconds').val(0);
+			//addZero();
+		}
+	})
+
+	$('#minusBreak').on("click", function() {
+		$('#sessionText').html('Break Time');
+		var x = $('#break').val();
+		if(x > 0) {
+			$('#break').val(x - 1);
+			$('#minutes').val(parseInt($('#break').val()));
+			$('#seconds').val(0);
+			//addZero();
+		}
+	})
+
 	$('#plusSession').on("click", function() {
-		$('#session').val(parseInt($('#session').val()) + 1);
+		var x = $('#session').val();
+		if(x < 59) {
+			$('#session').val(parseInt($('#session').val()) + 1);
+			$('#minutes').val(parseInt($('#session').val()));
+			$('#seconds').val(0);
+			//addZero();
+		}
 	})
 
 	$('#minusSession').on("click", function() {
 		var x = $('#session').val()
 		if(x > 0) {
 			$('#session').val(parseInt(x) - 1);
+			$('#minutes').val(parseInt($('#session').val()));
+			$('#seconds').val(0);
+			//addZero();
 		}
 	})
 
-	$('#plusBreak').on("click", function() {
-		$('#break').val(parseInt($('#break').val()) + 1);
-	})
+	function timer() {
 
-	$('#minusBreak').on("click", function() {
-		var x = $('#break').val();
-		if(x>0) {
-			$('#break').val(x - 1);
+		addZero();
+
+		var x = $('#seconds').val();
+
+		if(x == 0) {
+			$('#seconds').val(59);
+			$('#minutes').val($('#minutes').val() - 1);
+			addZero();
 		}
-	})
+		else{
+			$('#seconds').val(x - 1);
+		}
+		setTimeout(timer, 1000);
+	}
+
+	function addZero() {
+		if($('#minutes').val() < 10) {
+			$('#minutes').val('0' + $('#minutes').val()[$('#minutes').val().length - 1]);
+		}
+		else if($('#seconds').val() < 10) {
+			$('#seconds').html('0' + $('#seconds').val());
+		}
+	}
 })
